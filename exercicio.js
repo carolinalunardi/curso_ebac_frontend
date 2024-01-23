2b57e0cae88f6ce8b6a0f29bb7fbedc74602cbfe
Carolina Lunardi <lunardicll@gmail.com>
2024-01-23 15:49:46 +0000

    exercicio_ts

---
+++ exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(n1, n2) {
+    return n1 * n2;
+}
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(multiplicar(2, 10));
+console.log(saudacao("Carolina"));
---
+++ exercicio.ts
@@ -0,0 +1,10 @@
+function multiplicar(n1: number, n2: number): number {
+    return n1 * n2;
+}
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+console.log(multiplicar(2, 10))
+console.log(saudacao("Carolina"))
